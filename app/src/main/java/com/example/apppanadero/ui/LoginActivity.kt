3e65835667f408d63c92503a82af351f6e712bbb
Ruben <ruben.fermat@iesjulianmarias.es>
2026-04-21 00:21:55 +0200

    ver1.2

--- app/src/main/java/com/example/apppanadero/ui/LoginActivity.kt
+++ app/src/main/java/com/example/apppanadero/ui/LoginActivity.kt
@@ -4,22 +4,25 @@ import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.example.apppanadero.databinding.ActivityAdminHomeBinding
 import com.example.apppanadero.databinding.ActivityHomeBinding
 import com.example.apppanadero.databinding.ActivityLoginBinding
+import com.example.apppanadero.databinding.FragmentAdminPedidosBinding
 import com.example.apppanadero.databinding.FragmentDetallePedidoBinding
 import com.example.apppanadero.databinding.FragmentNuevoPedidoBinding
 import com.example.apppanadero.databinding.FragmentRegistroBinding
+import com.example.apppanadero.databinding.ItemPedidoAdminBinding
 import com.example.apppanadero.databinding.ItemPedidoBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: FragmentDetallePedidoBinding//Declaracion binding
+    private lateinit var binding: ItemPedidoAdminBinding//Declaracion binding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Iniciamos bindin
-        binding = FragmentDetallePedidoBinding.inflate(layoutInflater)
+        binding = ItemPedidoAdminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
