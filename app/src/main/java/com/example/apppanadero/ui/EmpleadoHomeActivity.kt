4bf8a9308fe46e73ecc38901c9c125973f00298e
RubenMMX <ruben.fermat@iesjulianmarias.es>
2026-05-21 14:38:03 +0200

    ver 2.4
    Added toolbar navigation and menu handling for logout and profile options.

--- app/src/main/java/com/example/apppanadero/ui/EmpleadoHomeActivity.kt
+++ app/src/main/java/com/example/apppanadero/ui/EmpleadoHomeActivity.kt
@@ -8,10 +8,7 @@ import com.example.apppanadero.databinding.ActivityEmpleadoHomeBinding
 class EmpleadoHomeActivity: AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityEmpleadoHomeBinding
-
-
-
+     private lateinit var binding: ActivityEmpleadoHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +16,107 @@ class EmpleadoHomeActivity: AppCompatActivity() {
         binding = ActivityEmpleadoHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbarAdmin)
 
+        //Navegacion atras de la toolbar
+        binding.toolbarAdmin.setNavigationOnClickListener {
+
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+    }
+
+
+
+    // CREAR MENÚ
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_home, menu)
+        return true
+    }
+
+    // MANEJAR CLICKS
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        //  Obtener NavController correctamente
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_admin) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+
+
+
+        return when (item.itemId) {
+
+            R.id.menu_home -> {
+                // Evitar navegar si ya estás en Home
+                if (navController.currentDestination?.id != R.id.adminHomeFragment) {
+                    navController.popBackStack(R.id.adminHomeFragment, false)
+                }
+                true
+            }
+
+            R.id.menu_perfil -> {
+                // navController.navigate(R.id.perfilFragment)
+                true
+            }
+
+           R.id.menu_logout -> {
+
+            // ------------------------------------------------
+            // FIREBASE LOGOUT
+            // ------------------------------------------------
+        
+            val repository = UsuarioRepository()
+        
+            repository.logout()
+        
+            // ------------------------------------------------
+            // SHARED PREFERENCES
+            // ------------------------------------------------
+        
+            val preferencias = getSharedPreferences(
+        
+                "sesion",
+        
+                MODE_PRIVATE
+            )
+        
+            val editor = preferencias.edit()
+        
+            editor.putBoolean(
+        
+                "mantener_sesion",
+        
+                false
+            )
+        
+            editor.commit()
+        
+            // ------------------------------------------------
+            // VOLVER LOGIN
+            // ------------------------------------------------
+        
+            startActivity(
+        
+                Intent(
+        
+                    this,
+        
+                    LoginActivity::class.java
+                )
+            )
+        
+            finish()
+        
+            true
+        }
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
